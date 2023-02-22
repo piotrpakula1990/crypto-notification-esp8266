f9c0b909b931c12a19828c982e07127c9ed04c8e
Piotr Pakuła <mpa.controllers.ppakua@gmail.com>
2023-02-22 12:11:46 +0100

    Init espressif project.

---
+++ Makefile
@@ -0,0 +1,3 @@
+PROJECT_NAME := crypto-notification-esp8266
+
+include $(IDF_PATH)/make/project.mk
---
+++ main/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "esp_spi_flash.h"
+
+void app_main()
+{
+    printf("Hello world!\n");
+}
